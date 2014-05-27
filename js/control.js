fbc2417c9702666e715253fb285e6ed4f2858fbd
Daryl Sew <darylsew@gmail.com>
2014-05-26 21:37:53 -0400

    Gravity and following mousex works.

--- js/control.js
+++ js/control.js
@@ -26,9 +26,6 @@ window.requestAnimFrame = function() {
 function drawCircle(ctx, x, y, r) {
     ctx.fillStyle = "rgba(0, 200, 200, 0.5)";
     ctx.beginPath();
-    var x = 25; // init x for bunny
-    var y = 25; // init y for bunny
-    var r = 20; // temp arc radius for bunny
     ctx.arc(x, y, r, 0, 2*Math.PI, true);
     ctx.fill();
 }
@@ -45,21 +42,18 @@ function getMousePos(canvas, e) {
 // c - html canvas (i.e. view)
 function animate(w, canvas) {
     w.timestep = w.time - w.startTime;
-//    console.log("Timestep: " + w.timestep);
-//    console.log("Bunny coords: (" + w.objects[0].x + ", " + w.objects[0].y + ")");
+    //console.log("Timestep: " + w.timestep);
+    //console.log("Bunny coords: (" + w.objects[0].x + ", " + w.objects[0].y + ")");
 
     // get the graphics context from the canvas
     // this is an object that lets us draw onto the canvas
     var ctx = canvas.getContext("2d");
     // clear context to draw the new stuff
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // render our world representation onto the view
     renderWorld(w, ctx);
 
-//    console.log("World before: " + w.objects[0].y);
     // make updates to the world - time, gravity, etc
     updateWorld(w);
-//    console.log("World after: " + w.objects[0].y);
 
     // call animate again with the updated world and context
     // in other words, request a new animation frame once the current one
@@ -72,23 +66,19 @@ function animate(w, canvas) {
 // for any updates to our world
 function updateWorld(w) {
     w.time = Date.now();
-    // TODO verify
     // loop through objects, update positions
     // x = vit + at^2
     w.objects.forEach(function (o) {
-        console.log("velb4: " + o.v);
         o.v -= w.acceleration;
-        o.y -= o.v;
+        o.y += o.v;
     });
-    return w;
 }
 
-// for rendering our world to the view
+// for rendering the world to the view
 function renderWorld(w, ctx) {
-    // TODO verify
     // loop through objects, draw each
-    // for now, draw circles; later we'll draw images
     w.objects.forEach(function (o) {
+        console.log("Coords plotting: (" + o.x + ", " + o.y + ")");
         drawCircle(ctx, o.x, o.y, 20);
     });
 }
@@ -115,10 +105,8 @@ $(document).ready(function() {
         startTime : Date.now(),
         time : Date.now(),
         objects : [bunny], 
-        acceleration : 10
+        acceleration : .0001
     };
-
-    console.log("bunny vel here: " + world.objects[0].v);
 
     animate(world, canvas);
 });
